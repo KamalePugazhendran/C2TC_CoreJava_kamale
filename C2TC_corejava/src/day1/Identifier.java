3b7622113d2fe2d4c2d9c97a6fd63b2958294072
hp <hp@DESKTOP-MA6VDTJ>
2025-07-22 10:02:30 +0530

    program1

---
+++ C2TC_corejava/src/day1/Identifier.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Identifier {
+	
+		public static void main(String[] args) {
+			int $number = 20;
+			System.out.println("value of the number variable is :"+$number);
+			String studentName="kamale";
+			System.out.println("value is: "+ studentName);
+		}
+
+	}
+
+
---
+++ C2TC_corejava/src/day2/Forloop.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Forloop {
+		public static void main(String[] args) {
+			for(int i = 1; i<=20; i++) {
+				System.out.println("Value of i:");
+				System.out.println(i);
+			}
+					
+		}
+
+	}
+
+
