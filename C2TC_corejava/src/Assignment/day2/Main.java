18d07644dc0fb813d2822243a31c10b0932bec9f
hp <hp@DESKTOP-MA6VDTJ>
2025-07-24 15:23:00 +0530

    corejava

--- C2TC_corejava/src/Assignment/day2/Main.java
+++ C2TC_corejava/src/Assignment/day2/Main.java
@@ -1,4 +1,4 @@
-package Assignment2;
+package Assignment.day2;
 
 public class Main {
 		public static void main(String[] args) {
--- C2TC_corejava/src/Assignment/day2/Student.java
+++ C2TC_corejava/src/Assignment/day2/Student.java
@@ -1,4 +1,4 @@
-package Assignment2;
+package Assignment.day2;
 
 public class Student {
 		public Student() {
