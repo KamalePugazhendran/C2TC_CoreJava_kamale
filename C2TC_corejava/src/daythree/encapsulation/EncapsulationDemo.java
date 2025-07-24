c6fb50d5bfd2849535b3c6ed08af52dfea5b4401
hp <hp@DESKTOP-MA6VDTJ>
2025-07-24 13:54:13 +0530

    daythree

---
+++ C2TC_corejava/src/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package daythree.encapsulation;
+
+public class EncapsulationDemo {
+	
+		public static void main(String[] args) {
+			
+			//object creation 
+			OopsConceptDemo obj = new OopsConceptDemo();
+			obj.setSerialNum(101);
+			obj.setName("kamale");
+			obj.setAge(20);
+			System.out.println(obj);
+			
+		}
+
+	}
+
+
