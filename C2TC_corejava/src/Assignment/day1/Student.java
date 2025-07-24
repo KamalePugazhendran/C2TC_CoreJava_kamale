d1071a742e8f4b5c25a84510fdc8f1bbe72a9b29
hp <hp@DESKTOP-MA6VDTJ>
2025-07-24 14:16:30 +0530

    Entityassignment

---
+++ C2TC_corejava/src/Assignment/day1/Student.java
@@ -0,0 +1,23 @@
+package Assignment.day1;
+
+public class Student {
+		
+		    private int id;
+		    private String name;
+		    private String department;
+
+		    // Constructor
+		    public Student(int id, String name, String department) {
+		        this.id = id;
+		        this.name = name;
+		        this.department = department;
+		    }
+
+		    // Method to display student details
+		    public void displayDetails() {
+		        System.out.println("ID: " + id + ", Name: " + name + ", Department: " + department);
+		    }
+		}
+
+		
+
