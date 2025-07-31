ce0add74f712c43c78a141dad8a2d65e58e81566
hp <hp@DESKTOP-MA6VDTJ>
2025-07-31 11:36:11 +0530

    corejava

---
+++ C2TC_corejava/src/Assignment5/Main.java
@@ -0,0 +1,49 @@
+package Assignment5;
+	import java.util.Scanner;
+
+	public class Main {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        String[] ticketDetails = sc.nextLine().split(",");
+	        String stageEvent = ticketDetails[0].trim();
+	        String customer = ticketDetails[1].trim();
+	        int noOfSeats = Integer.parseInt(ticketDetails[2].trim());
+
+	        TicketBooking booking = new TicketBooking(stageEvent, customer, noOfSeats);
+
+	        int choice = Integer.parseInt(sc.nextLine());
+
+	        switch (choice) {
+	            case 1:
+	                double amountCash = Double.parseDouble(sc.nextLine());
+	                booking.displayDetails();
+	                booking.makePayment(amountCash);
+	                break;
+
+	            case 2:
+	                double amountWallet = Double.parseDouble(sc.nextLine());
+	                String walletNumber = sc.nextLine().trim();
+	                booking.displayDetails();
+	                booking.makePayment(amountWallet, walletNumber);
+	                break;
+
+	            case 3:
+	                String holderName = sc.nextLine().trim();
+	                double amountCard = Double.parseDouble(sc.nextLine());
+	                String creditCardType = sc.nextLine().trim();
+	                String ccv = sc.nextLine().trim();
+	                booking.displayDetails();
+	                booking.makePayment(holderName, amountCard, creditCardType, ccv);
+	                break;
+
+	            default:
+	                System.out.println("Invalid choice");
+	        }
+
+	        sc.close();
+	    }
+	}
+
+
+
